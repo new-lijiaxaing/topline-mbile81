26411ff08aaa1121e6b4df158d3e80fb952d4f42
new-lijiaxing <2191729114@qq.com>
2019-09-03 21:59:41 +0800

    登录功能-封装发送登录的请求

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -32,10 +32,15 @@ export default {
     //   点击按钮，处理登录
     async handleLogin () {
       try {
-        const res = await login(this.user)
-        console.log(res)
+        // data 就是接口返回数据中的data（因为响应拦截器做了处理）
+        const data = await login(this.user)
+        console.log(data)
+        // 存储登录的状态
+        // 跳转到首页
+        this.$router.push('/')
+        this.$toast.success('登陆成功')
       } catch (err) {
-        console.log(err)
+        this.$toast.fail('登陆失败')
       }
     }
   }
