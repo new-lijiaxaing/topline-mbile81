079b1bbafcece0caa3b0281cf35f7345f00f6f7f
new-lijiaxing <2191729114@qq.com>
2019-09-04 16:11:55 +0800

    配置home的子路由

---
+++ src/views/Tabbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <router-vie></router-vie>
+      这是啥
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      homme
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
