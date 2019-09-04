4ca44d7e0b38e23d90cc39dff614c351f6e248f0
new-lijiaxing <2191729114@qq.com>
2019-09-04 11:38:09 +0800

    本地化（提示文字汉化）

--- src/main.js
+++ src/main.js
@@ -5,9 +5,14 @@ import store from './store'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 import './styles/index.less'
-import VeeValidate from 'vee-validate'
+import VeeValidate, { Validator } from 'vee-validate'
+// 导入VeeValiidate的中文包
+import zhCN from 'vee-validate/dist/locale/zh_CN'
 // 配置插件VeeValidate
 Vue.use(VeeValidate)
+// 配置中文
+Validator.localize('zhCN', zhCN)
+
 Vue.use(Vant)
 
 Vue.config.productionTip = false
