77ac0c2d593974388f1b825f3fa75bfbed6e5948
new-lijiaxing <2191729114@qq.com>
2019-09-04 16:26:35 +0800

    tabbar组件

--- src/views/Tabbar.vue
+++ src/views/Tabbar.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
       <router-vie></router-vie>
-      这是啥
+      <van-tabbar route>
+          <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+          <van-tabbar-item to="/qa" icon="search">问答</van-tabbar-item>
+          <van-tabbar-item to="/video" icon="friends-o">视频</van-tabbar-item>
+          <van-tabbar-item to="/user" icon="setting-o">我的</van-tabbar-item>
+      </van-tabbar>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'Tabbar'
 }
 </script>
 
