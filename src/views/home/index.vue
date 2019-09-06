a53d738d0005e27390276487aa82c3f46d2fd471
new-lijiaxing <2191729114@qq.com>
2019-09-06 16:35:03 +0800

    改造首页中的频道列表

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -85,6 +85,7 @@ import Vue from 'vue'
 import { Lazyload } from 'vant'
 // 加载moreaction组件
 import MoreAction from './components/MoreAction'
+import { getItem, setItem } from '@/utils/localStorage'
 // options 为可选参数，无则不传
 Vue.use(Lazyload)
 
@@ -125,10 +126,25 @@ export default {
     // 加载频道列表
     async loadChannels () {
       try {
-        const data = await getDefaultOrUserChannels()
+        let channels = []
+        // 1. 如果用户登录，发送请求，获取数据
+        if (this.$store.state.user) {
+          const data = await getDefaultOrUserChannels()
+          channels = data.channels
+        } else {
+          // 2. 如果用户没有登录，先去本地存储中获取数据，如果没有数据再发送请求
+          // 如果本地存储中没有值，获取的是null
+          channels = getItem('channels')
+          if (!channels) {
+            const data = await getDefaultOrUserChannels()
+            channels = data.channels
+            // 存储到本地存储中
+            setItem('channels', channels)
+          }
+        }
         // console.log(data)
         // 给所有的频道设置，时间戳和文章数组
-        data.channels.forEach((channel) => {
+        channels.forEach((channel) => {
           channel.timestamp = null
           channel.articles = []
           // 上拉加载
@@ -137,7 +153,7 @@ export default {
           // 下拉加载
           channel.pullLoading = false
         })
-        this.channels = data.channels
+        this.channels = channels
       } catch (err) {
         console.log(err)
       }
