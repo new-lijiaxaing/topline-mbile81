91b73d421a3fb3eafcffb4bd33df4de53b218f16
new-lijiaxing <2191729114@qq.com>
2019-09-08 19:33:26 +0800

    搭建列表组件

--- src/views/SearchResult.vue
+++ src/views/SearchResult.vue
@@ -1,17 +1,55 @@
 <template>
   <div>
-      <van-nav-bar title="搜索结果" left-text="返回" left-arrow @click-left="$router.back()"/>
-      {{q}}
+      <van-nav-bar title="搜索结果" left-text="返回" left-arrow fixed @click-left="$router.back()"/>
+       <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell
+        v-for="item in list"
+        :key="item"
+        :title="item"
+      />
+    </van-list>
   </div>
 </template>
 
 <script>
+import { setTimeout } from 'timers'
 export default {
   name: 'SearchResult',
-  props: ['q']
+  props: ['q'],
+  data () {
+    return {
+      list: [],
+      loading: false,
+      finisged: false
+    }
+  },
+  methods: {
+    onLoad () {
+      //   异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.van-list {
+  margin-top: 46px;
+}
 
 </style>
